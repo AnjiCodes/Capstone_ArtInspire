0a5bfcb429909e0e6b797f11ee7623dd6c154b50
AnjiCodes <n01579648@humber.ca>
2023-10-30 21:49:00 -0400

    index.js changed AGAIN

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ const connectDB=async()=>{
 dotenv.config()
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({origin:"http://localhost:5173",credentials:true}))
+app.use(cors({origin:"https://65405c4ab9c208386a03cbcd--musical-zuccutto-16466e.netlify.app",credentials:true}))
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
 app.use("/api/notes",noteRoute)
